ccf00bd64d2c5f2256afae61859d74e13bf5699e
laureanray <laureanraybahala@gmail.com>
2020-02-27 02:35:55 +0800

    added forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './modules/account/components/login/login.component';
+import {RegisterComponent} from './modules/account/components/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
