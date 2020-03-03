5a71ac7a1e42a3071065d079bdc4a24e9791f82e
laureanray <laureanraybahala@gmail.com>
2020-03-03 16:33:43 +0800

    add auth guard to protected routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './modules/account/login/login.component';
 import {RegisterComponent} from './modules/account/register/register.component';
 import {NotFoundComponent} from '@app/shared/not-found/not-found.component';
@@ -7,19 +7,25 @@ import {HomeComponent} from '@app/shared/home/home.component';
 import {AuthGuard} from '@app/core/authentication/auth.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'admin', loadChildren: () => import('@app/modules/admin/admin.module').then(m => m.AdminModule), canActivate: [AuthGuard]},
-  { path: 'instructor', loadChildren: () => import('@app/modules/instructor/instructor.module')
-      .then(m => m.InstructorModule)},
-  { path: 'student', loadChildren: () => import('@app/modules/student/student.module')
-      .then(m => m.StudentModule)},
-  { path: '**', component: NotFoundComponent}
-  ];
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'admin', loadChildren: () => import('@app/modules/admin/admin.module').then(m => m.AdminModule), canActivate: [AuthGuard]},
+  {
+    path: 'instructor', loadChildren: () => import('@app/modules/instructor/instructor.module')
+      .then(m => m.InstructorModule), canActivate: [AuthGuard]
+  },
+  {
+    path: 'student', loadChildren: () => import('@app/modules/student/student.module')
+      .then(m => m.StudentModule), canActivate: [AuthGuard]
+  },
+  {path: '**', component: NotFoundComponent}
+];
 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
