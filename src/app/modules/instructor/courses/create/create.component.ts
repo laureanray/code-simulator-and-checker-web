89d8d3df1d62225b47f5782e448522eb97c8f67c
laureanray <laureanraybahala@gmail.com>
2020-03-06 17:37:41 +0800

    create course

--- src/app/modules/instructor/courses/create/create.component.ts
+++ src/app/modules/instructor/courses/create/create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseNavService} from '@app/core/services/course.nav.service';
+import {faPlus, faSave } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-create',
@@ -7,12 +8,20 @@ import {CourseNavService} from '@app/core/services/course.nav.service';
   styleUrls: ['./create.component.sass']
 })
 export class CreateComponent implements OnInit {
+  faPlus = faPlus;
+  faSave = faSave;
+
+  moduleNumber = 1;
 
   constructor(private courseNavService: CourseNavService) {
     this.courseNavService.updateRoute('Create');
   }
 
   ngOnInit(): void {
+  }
+
+  addModule() {
+    this.moduleNumber++;
   }
 
 }
