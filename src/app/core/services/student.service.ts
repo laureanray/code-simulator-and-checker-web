cbd1e7ec87a2912ba0c5e74d0fc0f8c3027055af
laureanray <laureanraybahala@gmail.com>
2020-03-06 18:46:36 +0800

    added register method in student service

--- src/app/core/services/student.service.ts
+++ src/app/core/services/student.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '@environments/environment';
 import {map} from 'rxjs/operators';
+import {Student} from '@app/core/models';
 
 @Injectable({ providedIn: 'root'})
 export class StudentService {
@@ -12,5 +13,9 @@ export class StudentService {
   getStudent(username: string) {
     return this.http
       .get(`${environment.apiUrl}/api/v1/students/find/${username}`);
+  }
+
+  registerStudent(student: Student) {
+    return this.http.post(`${environment.apiUrl}/api/v1/students/register/`, student);
   }
 }
